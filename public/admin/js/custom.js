64a1a9595352bde0e96fd7ece074a2ffa510f363
Webert Maximiano <webertcoach@gmail.com>
2023-02-21 16:03:10 -0300

    Assets do Public

---
+++ public/admin/js/custom.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+    $(".nav-item").removeClass("active");
+    $(".nav-link").removeClass("active");
+    //check Admin Password is correct or not
+    $("#current_password").keyup(function() {
+        var current_password = $("#current_password").val();
+        //alert(current_password);
+        $.ajax({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            type: 'post',
+            url:  '/admin/check-admin-password',
+            data: {current_password:current_password},
+            success:function(resp){
+              // alert(resp);
+                if(resp=="false") {
+                    $("#check_password").html("<font color='red'>Current Password is Incorrect!</font>");
+                }else if(resp=="true"){
+                    $("#check_password").html("<font color='green'>Current Password is Correct!</font>");
+                }
+            },error:function(){
+                alert('Error');
+            }
+        });
+    })
+
+    //Update Admin Status ativo ou inativo ao clicar ele altera no BD.
+    $(document).on("click", ".updateAdminStatus", function() {
+        var status = $(this).children("i").attr("status");
+        var admin_id = $(this).attr("admin_id");
+        //alert(admin_id);
+        $.ajax({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            },
+            type: 'post',
+            url: '/admin/update-admin-status',
+            data: {status:status,admin_id:admin_id},
+            success:function(resp){
+                //alert(resp);
+                if(resp['status']==0) {
+                    $("#admin-"+admin_id).html("<i style='font-size:25px;' class='mdi mdi-bookmark-outline' status='Inactive'></i>");
+                } else if (resp['status']==1) {
+                    $("#admin-"+admin_id).html("<i style='font-size:25px;' class='mdi mdi-bookmark-check' status='Active'></i>");
+                }
+            }, error:function() {
+                alert("Error");
+            }
+        })
+    });
+});
